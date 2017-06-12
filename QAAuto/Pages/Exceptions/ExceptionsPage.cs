4fe6a250172a436eb740265da8d9fc427764d721
sliovino <siovino@makingsense.com>
2017-06-12 14:50:49 -0300

    Add exceptions elements and actions

--- QAAuto/Pages/Exceptions/ExceptionsPage.cs
+++ QAAuto/Pages/Exceptions/ExceptionsPage.cs
@@ -1,4 +1,6 @@
-﻿using QAAuto.Pages.Common;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using QAAuto.Pages.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +11,46 @@ namespace QAAuto.Pages.Exceptions
 {
     public class ExceptionsPage : AdvancedPage
     {
+        //Map
+        #region Exceptions Page Elements
+
+        [FindsBy(How = How.CssSelector, Using = "arc-navigation-button[ng-reflect-label='Manage exceptions']")]
+        public IWebElement manageExceptionsButton { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "div.main-content div.value")]
+        public IWebElement percentageText { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "div.main-content div.sub-text")]
+        public IWebElement dayLeftText { get; set; }        
+
+        #endregion
+
         WebUser webUser;
         public ExceptionsPage(WebUser webUser) : base(webUser)
         {
             this.webUser = webUser;
         }
+
+        #region Exceptions Actions
+
+        public void ClickManageExceptionsButton()
+        {
+            log.Info("Clicking manage exceptions button");
+            manageExceptionsButton.Click();
+        }
+
+        public string GetPercentageText()
+        {
+            log.Info("Getting percentage text");
+            return percentageText.Text;
+        }
+
+        public string GetDayLeftText()
+        {
+            log.Info("Getting day left text");
+            return dayLeftText.Text;
+        }
+
+        #endregion
     }
 }
